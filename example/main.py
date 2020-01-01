b27e9fe77fc7d0885a18175edcf0aaff67bb8386
Googler <knowbeeinc@gmail.com>
2020-01-01 21:20:17 +0200

    way faster

--- example/main.py
+++ example/main.py
@@ -1,18 +1,18 @@
 vow=["a", "e", "i", "o", "u"]
 
-stri = "Recommended"
+letter = "recommended"
+stri = letter.lower() 
 result=''
-
 for l in list(stri):
   for a in vow:
-    if l in vow and a=="a":
+    if l not in vow:
       stri = "".join(stri.split(a))
-    elif l in vow and a == "e":
-      stri = "".join(stri.split(a))
-    elif l in vow and a=="i":
-      stri = "".join(stri.split(a))
-    elif l in vow and a=="o":
-      stri = "".join(stri.split(a))
-    elif l in vow and a=="u": 
-      result = "".join(stri.split(a))
-print(result)
+    # elif l in vow and a == "e":
+    #   stri = "".join(stri.split(a))
+    # elif l in vow and a=="i":
+    #   stri = "".join(stri.split(a))
+    # elif l in vow and a=="o":
+    #   stri = "".join(stri.split(a))
+    # elif l in vow and a=="u": 
+    #   result = "".join(stri.split(a))
+print(stri)
